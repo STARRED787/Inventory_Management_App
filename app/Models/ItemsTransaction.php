7fa1fd487683965d7a37004c9683c59739d30701
STARRED787 <malithamalshan7@gmail.com>
2026-01-13 23:54:34 +0530

    Modal file to Itemstransaction table create

---
+++ app/Models/ItemsTransaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InventoryTransaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'type',
+        'quantity',
+    ];
+
+    // Relation: each transaction belongs to an item
+    public function item()
+    {
+        return $this->belongsTo(Item::class, 'item_id');
+    }
+}
